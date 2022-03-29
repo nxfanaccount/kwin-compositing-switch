ab527c8b4a83500c8cf2710394a38a2cf2cc569d
nxf <76963435+nxfanaccount@users.noreply.github.com>
2022-03-29 22:44:11 +0200

    Update compositing-switch.sh

--- compositing-switch.sh
+++ compositing-switch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 check=$([[ $(qdbus org.kde.KWin /KWin supportInformation | grep 'Compositing is not active') == '' ]] && echo 0 || echo 1)
+path="$(dirname "$BASH_SOURCE")/$(basename "$(realpath $0)")"
 
 if [[ $check == '0' ]]; then
 	qdbus org.kde.KWin /Compositor suspend
-	pkill yad; yad --notification --image="desktopdisconnected" & disown
+	pkill yad; yad --notification --image="desktopdisconnected" --command=$path & disown
 else
 	qdbus org.kde.KWin /Compositor resume
-	pkill yad; yad --notification --image="desktopconnected" & disown
+	pkill yad; yad --notification --image="desktopconnected" --command=$path & disown
 fi
